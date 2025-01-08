2b6d6616029ebc032f69caaf79dc1acce4e436eb
huzaifa <huzaifa@jenkins.com>
2025-01-09 00:34:47 +0100

    changed app.js to see if junit test fails

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy
+          Using Jenkins.js instead of React 
         </a>
       </header>
       <p>
